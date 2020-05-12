591f30f64e779bfcb1b6b219819785014b45d6a2
Igor Sushko <igor_sushko@yahoo.com>
2020-05-12 20:00:48 +0500

    Реализовал первый роут
    Доступ по localhost...   /login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageLoginComponent } from './page-login/page-login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
