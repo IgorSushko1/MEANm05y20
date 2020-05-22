ff01da4c7a4fdc6ab47a53e1b22775788ce27e8e
Igor Sushko <igor_sushko@yahoo.com>
2020-05-22 18:21:49 +0500

    Секция 8.  Отображение заказов
    Отображение всплывающего окна с итогом заказа

--- client/src/app/page-history/history-list/history-list.component.ts
+++ client/src/app/page-history/history-list/history-list.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 import { Order } from 'src/app/shared/interfaces';
+import { MaterialInstance, MaterialService } from 'src/app/shared/classes/material.classes';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.scss']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
 
   @Input() orders: Order[]
+  @ViewChild('modal') modalRef: ElementRef
+
+  modal: MaterialInstance
+  selectedOrder: Order
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.modal.destroy()
+  }
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.InitModal(this.modalRef)
   }
 
+  computePrice(order: Order): number {
+    return order.list.reduce(
+      (total, item) => {
+        return total += item.quantity * item.cost
+      }, 0)
+  }
+
+  selectOrder(order: Order) {
+    this.selectedOrder = order
+    this.modal.open()
+  }
+
+  closeModal() {
+    this.modal.close()
+  }
 }
