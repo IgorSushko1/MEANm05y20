e6beae74ae20906f0bf15a5f318b513e953d8c7b
Igor Sushko <igor_sushko@yahoo.com>
2020-05-24 21:27:50 +0500

    Секция 9. Вычисление данных аналитики

--- routes/analytic.js
+++ routes/analytic.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const passport = require('passport');
 const controller = require('../controllers/analytic')
 const router = express.Router();
 
 // localhost:5000/api/auth/login
-router.get('/overview', controller.overview)
+router.get('/overview', passport.authenticate('jwt', {
+  session: false
+}), controller.overview)
 
 // localhost:5000/api/auth/register
-router.get('/analytic', controller.analytic)
+router.get('/analytic', passport.authenticate('jwt', {
+  session: false
+}), controller.analytic)
 module.exports = router;
