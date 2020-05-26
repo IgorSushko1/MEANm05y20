fb6759497cd6674edfee01d371de5b6ec3d626c6
Igor Sushko <igor_sushko@yahoo.com>
2020-05-26 21:51:02 +0500

    fix package

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys.prod')
-} else {
-  module.exports = require('./keys.dev')
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys.prod')
+// } else {
+//   module.exports = require('./keys.dev')
+// }
+
+module.exports = {
+  mongoURI: 'mongodb+srv://IgorSushko1:JsuHRa4JP2HSNyC@cluster0-hxgwz.mongodb.net/test?retryWrites=true&w=majority',
+  jwt: 'dev-jwt' // jwt ключ для шифрования
 }
