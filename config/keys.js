a89be7d209b16ff3c25b0322c9857a3970f755aa
Igor Sushko <igor_sushko@yahoo.com>
2020-05-26 22:55:34 +0500

    without keys

--- config/keys.js
+++ config/keys.js
@@ -3,8 +3,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./keys.dev')
 }
-
-// module.exports = {
-//   mongoURI: 'mongodb+srv://IgorSushko1:JsuHRa4JP2HSNyC@cluster0-hxgwz.mongodb.net/test?retryWrites=true&w=majority',
-//   jwt: 'dev-jwt' // jwt ключ для шифрования
-// }
